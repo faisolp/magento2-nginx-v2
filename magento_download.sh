6a35d738a65d60bc302b003579ba6d7f00f1d94b
“Faisolp” <Faisolp@gmail.com>
2019-09-23 13:08:42 +0700

    init

---
+++ magento_download.sh
@@ -0,0 +1,3 @@
+#curl -k -O https://MAG005534028:23c9dd23965db1c6e781eecc1ba32b57e400a22d@www.magentocommerce.com/products/downloads/file/Magento-CE-2.3.1%2BSamples.tar.gz
+mkdir magento2
+tar zxvf ~/Downloads/Magento-CE-2.3.1-2019-03-18-06-28-13.tar.gz -C magento2/
